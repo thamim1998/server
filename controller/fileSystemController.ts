9232ed90185b422ca6d559736a20e136e06d221a
Thamimul Ansari <thamim1997@gmail.com>
2024-04-01 01:23:40 +0200

    delete get api complete

--- controller/fileSystemController.ts
+++ controller/fileSystemController.ts
@@ -1,43 +1,61 @@
 import { Request, Response } from "express";
 
 interface FileSystem {
- doc:{path:string, name:string, type:string}
+  doc: { path: string; docName: string; type: string };
 }
 
-let fileSystem = [{
-  path: `/`,
-  name: `Document`,
-  type: 'file'
-},
-{
-  path: `/`,
-  name: `Document2`,
-  type: 'file'
-},
-{
-  path: `/`,
-  name: `Document3`,
-  type: 'file'
-},
-{
-  path: `/`,
-  name: `Document`,
-  type: 'Folder'
-},
-{
-  path: `/Document`,
-  name: `Radar`,
-  type: 'folder'
-}];
+let fileSystem = [
+  {
+    path: `/`,
+    docName: `Document`,
+    type: "file",
+  },
+  {
+    path: `/`,
+    docName: `Document2`,
+    type: "file",
+  },
+  {
+    path: `/`,
+    docName: `Document3`,
+    type: "file",
+  },
+  {
+    path: `/`,
+    docName: `Document`,
+    type: "folder",
+  },
+  {
+    path: `/`,
+    docName: `Radar`,
+    type: "folder",
+  },
+];
 
 // Endpoint to retrieve all folders & files
-export function getAllFolder (req: Request, res: Response) {
-  res.json({ fileSystem });
-};
+export function getAllFolder(req: Request, res: Response) {
+  res.status(200).json(fileSystem);
+}
 
-export function addFolderOrFile (req: Request, res: Response) {
-  const { name, type, path } = req.body;
-  fileSystem.push({ path, name, type });
-  res.status(200).json({ message: 'File or folder added successfully.' });
-};
+export function addFolderOrFile(req: Request, res: Response) {
+  const { docName, type, path } = req.body;
+  fileSystem.push({ path, docName, type });
+  res.status(200).json({ message: "File or folder added successfully." });
+}
 
+export function getDocumentByPath(req: Request, res: Response) {
+  const { path } = req.query;
+  const elementsWithPathDoc = fileSystem.filter((item) => item.path === path);
+  res.status(200).json(elementsWithPathDoc);
+}
+
+export function deleteDocument(req: Request, res:Response){
+  const { docName, type, path } = req.body;
+  const index = fileSystem.findIndex(element => element.path === path && element.docName === docName && element.type === type);
+  if (index !== -1) {
+    fileSystem.splice(index, 1);
+    res.status(200).json({ message: 'Document deleted successfully' });
+  } else {
+    res.status(404).json({ message: 'Document not found' });
+  }
+}
--- src/route/filesystem.ts
+++ src/route/filesystem.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getAllFolder, addFolderOrFile } from "../../controller/fileSystemController";
+import { getAllFolder, addFolderOrFile, getDocumentByPath , deleteDocument} from "../../controller/fileSystemController";
 
 const router = Router()
 
 router.get("/fileSystem", getAllFolder);
+router.get("/fileSystem/doc", getDocumentByPath);
 router.post("/fileSystem/add", addFolderOrFile);
-// router.delete("/api/fileSystem/delete", fileController.deleteFolderOrFile);
+router.delete("/fileSystem/delete", deleteDocument);
 
 export default router;
